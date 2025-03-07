13c7ceb47296da6a00994d1526d2f2ebea8a23d3
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-03-07 11:38:20 -0500

    Protecting Routes

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,8 @@
 "use client";
-export default function Error() {
+export default function Error({ error }: { error: Error }) {
     return (
         <main className="flex flex-col justify-center min-h-screen h-full text-center gap-5 max-w-5xl mx-auto">
-            <h1 className="text-4xl font-bold">Something went wrong</h1>
+            <h1 className="text-4xl font-bold">{error.message}</h1>
         </main>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import {
+    ClerkProvider,
+    SignInButton,
+    SignUpButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
@@ -7,6 +15,13 @@ export default function Home() {
         <main className="flex flex-col justify-center min-h-screen h-full text-center gap-5 max-w-5xl mx-auto">
             <h1 className="text-4xl font-bold">InvoiceZen</h1>
             <p>
+                <SignedOut>
+                    <SignInButton />
+                    <SignUpButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
                 <Button asChild variant="outline">
                     <Link
                         href={"/dashboard"}
