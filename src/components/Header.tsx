09e2d523c399baf124baaaa3f991a150669ccb4d
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-03-11 18:23:51 -0400

    Created custom sign-in & sign-up pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,6 @@ export default function Header() {
                     <div>
                         <SignedOut>
                             <SignInButton />
-                            <SignUpButton />
                         </SignedOut>
                         <SignedIn>
                             <UserButton />
